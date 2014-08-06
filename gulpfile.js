4b221818230a8223aab2ec76136b26a472c735a0
Shane Osbourne <shane.osbourne8@gmail.com>
2014-08-06 08:04:35 +0100

    Added S3 build for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@ var sass        = require("gulp-ruby-sass");
 var minifyCSS   = require("gulp-minify-css");
 var rename      = require("gulp-rename");
 var prefix      = require("gulp-autoprefixer");
+var through2    = require("through2");
 var cp          = require("child_process");
+var rev         = require("gulp-rev");
+var awspublish  = require('gulp-awspublish');
+var yaml        = require('js-yaml');
+
 
 var messages = {
     jekyllBuild: "<span style=\"color: grey\">Running:</span> $ jekyll build"
@@ -49,7 +54,7 @@ gulp.task("browser-sync", ["sass", "build-dev"], function() {
  */
 gulp.task("sass", function () {
     browserSync.notify("Compiling SASS...");
-    gulp.src(["_scss/**/*.scss", "bower_components/pygments/css/*.scss"])
+    return gulp.src(["_scss/**/*.scss", "bower_components/pygments/css/*.scss"])
         .pipe(sass())
         .pipe(prefix(["last 5 versions", "> 1%", "ie 8"], { cascade: true }))
         .pipe(gulp.dest("_site/css"))
@@ -59,6 +64,53 @@ gulp.task("sass", function () {
         .pipe(rename("main.min.css"))
         .pipe(gulp.dest("css"));
 });
+
+
+/**
+ * REV css file
+ */
+gulp.task("rev:css", ['sass'], function () {
+
+    var publisher = awspublish.create(require("./.aws.json"));
+
+    var headers = {
+        'Cache-Control': 'max-age=315360000, no-transform, public',
+        'Expires': new Date(Date.now() + 63072000000).toUTCString()
+    };
+
+    function updateYaml(file, cb) {
+        var fs = require("fs");
+        try {
+            var doc = yaml.safeLoad(fs.readFileSync("./_config.yml", "utf-8"));
+            doc.cssFile = "https://s3-eu-west-1.amazonaws.com/shakshane/" + file.s3.path;
+            fs.writeFileSync("./_config.yml", yaml.safeDump(doc));
+        } catch (e) {
+            console.log(e);
+        }
+
+        cb(null);
+    }
+
+    return gulp.src("css/main.min.css")
+
+        .pipe(rev())
+        // gzip, Set Content-Encoding headers and add .gz extension
+        .pipe(awspublish.gzip())
+
+        // publisher will add Content-Length, Content-Type and  headers specified above
+        // If not specified it will set x-amz-acl to public-read by default
+        .pipe(publisher.publish(headers))
+
+        // create a cache file to speed up consecutive uploads
+        .pipe(publisher.cache())
+
+        // print upload updates to console
+        .pipe(through2.obj(function (file, enc, cb) {
+            updateYaml(file, cb);
+        }));
+});
+
+gulp.task("release:css", ['rev:css']);
 
 /**
  * Watch scss files for changes & recompile
